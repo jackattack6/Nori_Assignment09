a9ab39b0afde093bf9210b708d6e1eaf2021f187
Bill <Bill@DESKTOP-E57C9AG.wifi.uc.edu>
2022-10-25 18:03:26 -0400

    Doggie

--- Nori_Assignment09/DogPackage/Dog.py
+++ Nori_Assignment09/DogPackage/Dog.py
@@ -1,5 +1,16 @@
-class Dog
-    def __init__(self, color, size)
+'''
+Name: Bill Tummler
+email: N/A (Group Project)
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: Collaborate with peers to develop an Eclipse project modeling something 
+in the real world 
+Citations: N/A
+Anything else that's relevant: N/A
+'''
+class Dog():
+    def __init__(self, color, size):
         self.color = color
         self.size = size
  
@@ -13,7 +24,10 @@ class Dog
         self. color = color
         
     def __str__(self):
-        return f"{self.color} Dog that  is {self.size} size"
+        return f"{self.color} Dog that is {self.size} size"
     
     def __repr__(self):
         return f"Dog(\{self.color}\", \"{self.size}\")" 
+    
+    def walk(self):
+        print (f"the {self.color} dog of size {self.size} walked a litte")
